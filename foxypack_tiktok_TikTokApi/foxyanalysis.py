d9e4c2b142ade47aa1956fce399cc852c41e0b79
ivanabramov-dev <ivan.abramov@1win.vision>
2026-03-27 20:56:43 +0300

    feature: add foxyanalysis (todo: get_code)

---
+++ foxypack_tiktok_TikTokApi/foxyanalysis.py
@@ -0,0 +1,47 @@
+import re
+import urllib.parse
+from typing_extensions import override
+
+from foxypack import FoxyAnalysis, DenialAnalyticsException
+from foxypack_tiktok_TikTokApi.answers import (
+    TikTokAnswersAnalysis,
+    TikTokEnum,
+)
+
+
+class FoxyTikTokAnalysis(FoxyAnalysis):
+    @staticmethod
+    def clean_link(link: str) -> str:
+        parsed = urllib.parse.urlparse(link)
+        return f"{parsed.scheme}://{parsed.netloc}{parsed.path}"
+
+    @staticmethod
+    def get_type_content(link: str) -> str | None:
+        parsed = urllib.parse.urlparse(link)
+        path = parsed.path.rstrip('/')
+        
+
+        if re.match(r'@[\w\.-]+/video/(\d{18,21})', path) or re.match(r'/(t|v)/[\w]+', path):
+            return TikTokEnum.video.value
+
+        if re.match(r'^/@[\w\.-]+/?$', path):
+            return TikTokEnum.profile.value
+
+        return None
+
+    @staticmethod
+    def get_code(link: str) -> str:
+        return ''
+
+    @override
+    def get_analysis(self, url: str) -> TikTokAnswersAnalysis:
+        type_content = self.get_type_content(url)
+        if type_content is None:
+            raise DenialAnalyticsException(url)
+
+        return TikTokAnswersAnalysis(
+            url=self.clean_link(url),
+            social_platform="tiktok",
+            type_content=type_content,
+            code=self.get_code(url),
+        )
