c017e1d7b900d961fed528411766477f77a3e644
ivanabramov-dev <ivan.abramov@1win.vision>
2026-03-31 15:51:54 +0300

    feature: add Proxy class

--- foxypack_tiktok_TikTokApi/entities.py
+++ foxypack_tiktok_TikTokApi/entities.py
@@ -1,7 +1,7 @@
 from foxy_entities import SocialMediaEntity
-
+from .utils import Proxy
 
 class TikTokSession(SocialMediaEntity):
     ms_token: str
     browser: str
-    # proxy: str
+    proxy: Proxy | None
--- foxypack_tiktok_TikTokApi/utils.py
+++ foxypack_tiktok_TikTokApi/utils.py
@@ -3,6 +3,25 @@ from foxypack import AnswersAnalysis
 from foxypack_tiktok_TikTokApi.answers import TikTokAnswersAnalysis
 
 
+class Proxy:
+    server : str
+    username : str | None
+    password : str | None
+
+    def to_dict(self) -> dict:
+        
+        if self.username and self.password:
+            return {
+                "server": self.server,
+                "username": self.username,
+                "password": self.password
+            }
+        else:
+            return {
+                "server": self.server
+            }
+
+
 def as_tiktok_analysis(
     analysis: AnswersAnalysis,
 ) -> TikTokAnswersAnalysis:
